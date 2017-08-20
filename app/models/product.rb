9c1577ce7d8273d5ab135bf5e1f6513e97024c45
Mohamed Ibrahim <mohamed.mahmoud59@gmail.com>
2017-08-20 20:56:55 +0200

    Product reviews/rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
   has_many :bundle_products
   has_many :bundles, through: :bundle_products
+  has_many :reviews do
+    def average_rating
+      average(:rating).to_f
+    end
+  end
 
   scope :featured, ->{ where(featured: true) }
 end
---
+++ spec/factories/reviews.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :review do
+    rating 1
+    review "MyText"
+  end
+end
