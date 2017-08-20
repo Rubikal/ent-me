c8cd6ac5c486a79d4b0f3f3efd065cd459d80d76
Mohamed Ibrahim <mohamed.mahmoud59@gmail.com>
2017-08-20 20:54:11 +0200

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820175651) do
+ActiveRecord::Schema.define(version: 20170820185320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20170820175651) do
     t.string "type", null: false
     t.boolean "featured", default: false, null: false
     t.text "trailer"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bundle_products", "bundles"
