8901dc731f717ed17e0dc22e78ca55a0395a2a82
Moustafa Badawy <moustafa.badawym@gmail.com>
2017-08-20 16:45:22 +0200

    Disabled auto generation of assets & helpers

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module EntMe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Skip assets, scaffolds.css, helpers
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.stylesheets false
+    end
   end
 end
